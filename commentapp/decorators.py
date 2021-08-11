4026ff4f58e6cfb0fe41f8813bc35029a84fe65e
jsssj <eeun45@gmail.com>
2021-08-11 10:16:21 +0900

    8/11

--- commentapp/decorators.py
+++ commentapp/decorators.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseForbidden
 from commentapp.models import Comment
 
 
+
 def comment_ownership_required(func):
     def decorated(request, *args, **kwargs):
         target_comment = Comment.objects.get(pk=kwargs['pk'])
