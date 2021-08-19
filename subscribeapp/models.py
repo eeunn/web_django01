8bffc5fdf8e6e3a35041dd633e176eb20d1af2de
jsssj <eeun45@gmail.com>
2021-08-19 15:07:23 +0900

    8/19 -3

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription', null= False)
+
+    # 외부 설정 정보 - 구독은 1번만
+    class Meta:
+        unique_together = ['user', 'project']
+
+
