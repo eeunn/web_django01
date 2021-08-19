e179267311c46546520ab0bd42ec211aa252cda5
jsssj <eeun45@gmail.com>
2021-08-19 16:00:59 +0900

    8/19 -5

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -9,7 +9,7 @@ class Subscription(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='subscription', null=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE,
-                                related_name='subscription', null= False)
+                                related_name='subscription', null=False)
 
     # 외부 설정 정보 - 구독은 1번만
     class Meta:
