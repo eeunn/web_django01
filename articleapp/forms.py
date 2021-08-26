7a494fe8249e33852958b14ce085643724fc890c
eunjeang <eeun45@gmail.com>
2021-08-26 14:56:20 +0900

    8/26

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,16 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    # 스크립트 안에 .editable을 사용하게 하기 위해 설정
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable',
+                                                           'style': 'text-align: left;'
+                                                                    'min-height: 10rem;'}))
+
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
